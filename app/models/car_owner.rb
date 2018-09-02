e7f76ef3ead25db916b6b42b1df9eb9c35d405be
yapjinai <jinai.jyap@gmail.com>
2018-09-02 16:36:16 -0400

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,32 @@
 class CarOwner
+  @@all = []
 
-  attr_reader :name
+  attr_reader :name, :cars
 
   def initialize(name)
     @name = name
+    @cars = []
+
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.average_number_cars
+    @@all.map { |o| o.cars.count }.inject(:+) / @@all.count
+  end
+
+  def mechanics
+    @cars.map { |c| c.mechanic }
+  end
+
+  def fix_car(car, mechanic)
+    car.mechanic = mechanic
+    
+    mechanic.cars << car
+    mechanic.owners << self
+  end
 
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,22 @@
 class Mechanic
+  @@all = []
 
-  attr_reader :name, :specialty
+  attr_reader :name, :specialty, :cars, :owners
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @cars = []
 
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def owner_names
+    @owners.map { |o| o.name }
+  end
 
 end
