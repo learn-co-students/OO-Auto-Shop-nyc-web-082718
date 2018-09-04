a73552065349e534ff4662e28761d7fc251a5ff7
github username <github email address>
2018-09-04 01:02:09 -0400

    completed auto shop relationships

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -2,8 +2,38 @@ class CarOwner
 
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def my_cars
+    Car.all.select do |car_obj|
+      car_obj.owner == self
+    end
+  end
+
+  def my_mechanics
+    my_cars.map do |car_obj|
+      car_obj.mechanic
+    end
+  end
+
+  def self.average_cars
+    total_cars = 0
+    self.all.each do |owner_obj|
+      total_cars += owner_obj.my_cars.size
+    end
+    total_cars/self.all.size.to_f
+    #each owner has a number of cars
+    #add togethr the number of cars each owner has
+    #take average of sum
   end
 
 
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,10 +2,34 @@ class Mechanic
 
   attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  @@all = []
 
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def cars
+    Car.all.select do |car|
+      car.mechanic == self
+    end
+  end
+
+  def owners
+    self.cars.map do |car|
+      car.owner
+    end
+  end
+
+  def owner_names
+    self.owners.map do |owner_obj|
+      owner_obj.name
+    end
+  end
 
 end
