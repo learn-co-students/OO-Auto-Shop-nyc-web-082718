ea6903ddcced48401392e5fae75b90ecdf77c3d2
skarolk <skarolkiewicz@gmail.com>
2018-09-04 08:41:22 -0400

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,30 @@
 class Car
 
-  attr_reader :make, :model
+  attr_accessor :make, :model, :car_owner, :mechanic, :classification
 
-  def initialize(make, model, classification)
+  @@all = []
+
+  def initialize(make, model, classification, car_owner, mechanic)
     @make = make
     @model = model
     @classification = classification
+    @car_owner = car_owner
+    @mechanic = mechanic
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def mechanic_match
+    mechanic_match = []
+    Mechanic.all.each do |details|
+      if details.specialty == self.classification
+        mechanic_match << details
+      end
+    end
+    mechanic_match
   end
 
 end
--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -2,9 +2,47 @@ class CarOwner
 
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def owned
+    cars = []
+    Car.all.each do |details|
+      if details.car_owner == self
+        cars << details
+      end
+    end
+    cars
+  end
+
+  def mechanic
+    mechanic = []
+    Car.all.each do |details|
+      if details.car_owner == self
+        mechanic << details.mechanic
+      end
+    end
+    mechanic
+  end
+
+  def self.owned_average
+    cars_list = []
+    Car.all.each do |details|
+        cars_list << details
+    end
+    owner_list = []
+    @@all.each do |owners|
+      owner_list << owners
+    end
+    return cars_list.length/owner_list.length
+  end
 
 end
