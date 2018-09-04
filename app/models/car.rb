b0e447e80b71f534cea41fb85c76f515924162a8
yammik <mkim0514@gmail.com>
2018-09-04 00:53:47 -0400

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,36 @@
 class Car
 
-  attr_reader :make, :model
+  attr_reader :make, :model, :owner, :classification
+  attr_accessor :mechanic
 
-  def initialize(make, model, classification)
+  @@all = []
+
+  def initialize(make, model, classification, owner)
     @make = make
     @model = model
     @classification = classification
+    @owner = owner
+    @mechanic
+    @@all << self
+  end
+
+  # Get a list of all cars
+  def self.all
+    @@all
+  end
+
+  # Get a list of all car classifications
+  def self.classifications
+    self.all.map do |car|
+      car.classification
+    end.uniq
+  end
+
+  # Get a list of mechanics that have an expertise that matches the car classification
+  def mechanics
+    Mechanic.all.select do |mech|
+      mech.specialty == self.classification
+    end
   end
 
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,10 +2,38 @@ class Mechanic
 
   attr_reader :name, :specialty
 
+  @@all = []
+
 def initialize(name, specialty)
   @name = name
   @specialty = specialty
+  @@all << self
 end
 
+  # Get a list of all mechanics
+  def self.all
+    @@all
+  end
 
-end
+  # Get a list of all cars that a mechanic services
+  def client_cars
+    Car.all.select do |car|
+      car.mechanic == self
+    end
+  end
+
+  # Get a list of all the car owners that go to a specific mechanic
+  def my_clients
+    self.client_cars.map do |car|
+      car.owner
+    end
+  end
+
+  # Get a list of the names of all car owners who go to a specific mechanic
+  def my_clients_names
+    self.my_clients.map do |client|
+      client.name
+    end
+  end
+
+end # end Mechanic class
